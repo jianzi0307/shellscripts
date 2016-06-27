0f6d3c72210e12ec5d2f2c8e0c62088cb7c1021f
jianzi0307 <jianzi0307@gmail.com>
2016-06-27 10:19:31 +0800

    navis scripts

---
+++ linux_server_entry.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+ # 检查是否root用户
+if [ $(id -u) != "0" ]; then
+    echo "Error: You must be root to run this script."
+    exit 1
+fi
+yum update
+yum install -y screen dstat wget lrzsz git vim
+wget http://pkgs.repoforge.org/axel/axel-2.4-1.el6.rf.x86_64.rpm
+rpm -ivh axel-2.4-1.el6.rf.x86_64.rpm
+axel -n 10 http://soft.vpser.net/lnmp/lnmp1.2-full.tar.gz
+tar zxvf lnmp1.2-full.tar.gz
+cd lnmp1.2-full
+sh install.sh
